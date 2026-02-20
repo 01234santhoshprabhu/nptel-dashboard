0447e28239d74dc84899c7efa52b4e3fd52abb7c
01234santhoshprabhu <santhoshofficial70@gmail.com>
2026-02-20 11:24:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ st.markdown("""
 </style>
 """, unsafe_allow_html=True)
 
-st.title("📊 NPTEL Academic Analytics Dashboard")
+st.title("📊 NPTEL Analytics Dashboard")
 
 # ---------------- FILE UPLOAD ----------------
 uploaded_file = st.file_uploader("Upload CSV File", type=["csv"])
@@ -196,3 +196,4 @@ if uploaded_file:
 
 else:
     st.info("⬆ Upload CSV file to start analysis")
+
